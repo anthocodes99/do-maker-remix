cbc125783cfaa73e6b9cc130e2bf5b05ccff11a9
anthocodes99 <anthocodes99@users.noreply.github.com>
2024-09-16 00:43:14 +0800

    feat(routes): add new landing page
    modified title and description as well.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,52 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/.server/db";
+import { companies } from "~/.server/db/schema/companies";
+
+import {
+  Card,
+  // CardContent,
+  CardDescription,
+  // CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "DO Maker" },
+    { name: "description", content: "Delivery Order Maker!" },
   ];
 };
 
+export const loader = async () => {
+  const rows = await db().select().from(companies);
+  return {
+    data: rows,
+  };
+};
+
 export default function Index() {
+  const { data } = useLoaderData<typeof loader>();
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <main className="max-w-6xl mx-auto mt-12">
+        <Link to="/delivery-order">
+          <h1 className="text-2xl font-bold">Head to Delivery Order</h1>
+        </Link>
+        <section className="flex gap-4 mt-24">
+          {data.map((company) => (
+            <Link key={company.id} to={`/company/${company.id}`}>
+              <Card>
+                <CardHeader className="max-w-lg">
+                  <CardTitle>{company.name}</CardTitle>
+                  <CardDescription>{`${company.addressLineOne} ${company.addressLineTwo}`}</CardDescription>
+                </CardHeader>
+              </Card>
+            </Link>
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
